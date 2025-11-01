e25ae95f280e7244b03adc8ee4a668d4d4cf4a1f
Kalusha Aguti <kalushaaguti@gmail.com>
2025-10-31 20:24:54 -0500

    Update app_frontend.py

--- app_frontend.py
+++ app_frontend.py
@@ -7,19 +7,21 @@ movie_name = st.text_input("Enter a movie title:")
 
 if st.button("Recommend"):
     if movie_name.strip() != "":
-        url = "https://neural-movie-recommender.onrender.com/recommend" 
+        url = "https://neural-movie-recommender.onrender.com/recommend"
         params = {"title": movie_name}
 
         try:
-            response = requests.get(url, params=params)
+            response = requests.get(url, params=params, timeout=10)
             data = response.json()
 
-            if "error" in data:
-                st.error(data["error"])
+            if response.status_code != 200:
+                st.error(f"Backend error: {response.status_code}")
+            
             else:
                 st.subheader(f"🎥 Recommendations for: **{movie_name}**")
-                for movie in data["recommended_movies"]:
-                    st.write(f"✅ {movie}")
+                for movie in data["recommendations"]:
+                    st.write(f"✅ {movie['title']}")
 
         except Exception as e:
             st.error("🚨 Could not connect to backend API.")
+            st.write(str(e))
